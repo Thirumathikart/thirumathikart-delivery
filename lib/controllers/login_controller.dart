1cd5574c27d74e7657833dff35481b60af729894
DonWIck32 <nitish2k03@gmail.com>
2023-04-13 19:58:43 +0530

    feat(login): integrate login with backend

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,3 +1,58 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:thirumathikart_delivery/constants/navigation_routes.dart';
+import 'package:thirumathikart_delivery/models/login_request.dart';
+import 'package:thirumathikart_delivery/services/api_service.dart';
+import 'package:thirumathikart_delivery/services/storage_service.dart';
+class LoginController extends GetxController {
+  final api = Get.find<ApiServices>().api;
+  final storage = Get.find<StorageServices>();
+  final isLoading = false.obs;
+  final userNameTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+  var userToken = '';
 
-class LoginController extends GetxController {}
+  void login() async {
+    final userName = userNameTextController.text;
+    final password = passwordTextController.text;
+    if (userName.isEmpty || password.isEmpty) {
+      Get.snackbar('Invalid Credentials', 'Unable To Login');
+      return;
+    }
+    isLoading.value = true;
+    api
+        .loginDelivery(LoginRequest(email: userName, password: password), storage)
+        .then((response) {
+      isLoading.value = false;
+      navigateToHome();
+    }, onError: (err) {
+      isLoading.value = false;
+      Get.snackbar('Failed To Login', err.toString());
+    });
+  }
+
+  void navigateToHome() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Get.offNamed(NavigationRoutes.home);
+    });
+  }
+
+  void navigateToLogin() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Get.offAndToNamed(NavigationRoutes.loginRoute);
+    });
+  }
+
+  bool userTokenFetch() {
+    if (storage.retriveJWT() == null) {
+      userToken = '';
+    } else {
+      userToken = storage.retriveJWT()!;
+    }
+    if (userToken == '') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
